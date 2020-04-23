4280b492977df7e61b83e2e7a0d3075ecc965118
Lucas S. Guimarães <lucasasafe@outlook.com.br>
2020-04-23 18:35:46 -0300

    feat: add protype listview of the app

--- lib/main.dart
+++ lib/main.dart
@@ -37,65 +37,101 @@ class _MyHomePageState extends State<MyHomePage> {
     return Container(
         decoration: const BoxDecoration(
           gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment(0.8, 0.0),
             colors: <Color>[
-              Color(0xFF0D47A1),
-              Color(0xFF1976D2),
               Color(0xFF42A5F5),
+              Color(0xFF1976D2),
+              Color(0xFF0D47A1),
             ],
+            tileMode: TileMode.clamp,
           ),
         ),
         constraints: BoxConstraints.expand(
           height: Theme.of(context).textTheme.display1.fontSize * 1.1 + 200.0,
         ),
         alignment: Alignment.center,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text(
-                'Próximo evento',
-                style: Theme.of(context)
-                  .textTheme
-                  .display1
-                  .copyWith(
-                    color: Colors.white,
-                    fontWeight: FontWeight.w400
-                  )
-              ),
-              Text(
-                '$_counter',
-                style: Theme.of(context)
-                  .textTheme
-                  .display4
-                  .copyWith(color: Colors.white)
-              ),
-              Text(
-                'Sábado',
-                style: Theme.of(context)
-                  .textTheme
-                  .subtitle
-                  .copyWith(color: Colors.white),
-              ),
-              Text(
-                'Janeiro de 2020',
-                style: Theme.of(context)
-                  .textTheme
-                  .subtitle
-                  .copyWith(color: Colors.white),
-              ),
-              Text(
-                'AcampADAD Extreme',
-                style: Theme.of(context)
-                  .textTheme
-                  .display1
-                  .copyWith(
-                    fontSize: 14,
-                    color: Colors.white
-                  ),
-              ),
-          ],
-        ),
-        transform: Matrix4.rotationZ(0.1),
-      );
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Próximo evento',
+              style: Theme.of(context)
+                .textTheme
+                .display1
+                .copyWith(
+                  color: Colors.white,
+                  fontWeight: FontWeight.w400
+                )
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context)
+                .textTheme
+                .display4
+                .copyWith(color: Colors.white)
+            ),
+            Text(
+              'Sábado',
+              style: Theme.of(context)
+                .textTheme
+                .subtitle
+                .copyWith(color: Colors.white),
+            ),
+            Text(
+              'Janeiro de 2020',
+              style: Theme.of(context)
+                .textTheme
+                .subtitle
+                .copyWith(color: Colors.white),
+            ),
+            Text(
+              'AcampADAD Extreme',
+              style: Theme.of(context)
+                .textTheme
+                .display1
+                .copyWith(
+                  fontSize: 14,
+                  color: Colors.white
+                ),
+            ),
+        ],
+      ),
+      transform: Matrix4.rotationZ(0.0),
+    );
+  }
+
+  Widget buildListViewEvents() {
+
+    final List<String> entries = <String>['A', 'B', 'C', 'D', 'F', 'G', 'H'];
+    final List<int> colorCodes = <int>[600, 500, 400, 300, 200, 100, 0];
+
+    return ListView.separated(
+      padding: const EdgeInsets.all(8),
+      itemCount: entries.length,
+      itemBuilder: (BuildContext context, int index) {
+        return Container(
+          height: 50,
+          color: Colors.teal[colorCodes[index]],
+          child: Center(child: Text('Entry ${entries[index]}')),
+        );
+      },
+      separatorBuilder: (BuildContext context, int index) => const Divider(),
+    );
+  }
+
+  Widget buildBody(BuildContext context) {
+    return Container(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          buildNextEventContainer(context),
+          Expanded(
+            child: buildListViewEvents(),
+          ),
+        ],
+      ),
+    );
   }
 
   @override
@@ -104,7 +140,7 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: buildNextEventContainer(context),
+      body: buildBody(context),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
